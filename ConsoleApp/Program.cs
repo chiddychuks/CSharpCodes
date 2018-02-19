151ad9e39b107a8b56a07aaa371edde9346a348f
ChiddyChuks <greatchukwumachidi@gmail.com>
2018-02-19 18:33:01 +0100

    my first push

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,6 +10,19 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            String name = Console.ReadLine();
+            Console.WriteLine("Hi "+ name + ". Please enter the hours of sleep");
+            int number = int.Parse( Console.ReadLine());
+            if (number > 10)
+            {
+                Console.WriteLine("You are getting enough sleep. Thank you");
+            }
+            else
+            {
+                Console.WriteLine("You need enough sleep biko");
+            }
+            
+           
         }
     }
 }
